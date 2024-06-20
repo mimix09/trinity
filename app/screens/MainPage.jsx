d040ede990803c6d6e62af4c29c7bd47764109e3
mimix09 <118640742+mimix09@users.noreply.github.com>
2024-06-20 20:15:14 +0200

    first update

---
+++ app/screens/MainPage.jsx
@@ -0,0 +1,15 @@
+import "../style/mainPagestyl.css";
+
+function MainPage(){
+
+    return (
+        <div>
+            <div className="glown-menu">
+                <a href="">jeden</a>
+                <a href="">dwa</a>
+            </div>
+        </div>
+    );
+}
+
+export default MainPage;
