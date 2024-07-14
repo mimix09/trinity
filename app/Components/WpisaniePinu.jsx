d205141241f1f9e0e05aa59f5167a20308fce1a9
mimix09 <mpianka253@gmail.com>
2024-07-14 19:49:00 +0200

    jeden z pierwszych

--- app/Components/WpisaniePinu.jsx
+++ app/Components/WpisaniePinu.jsx
@@ -1,26 +1,65 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { db } from "../../src/firebase";
+import { collection, query, where, getDocs } from 'firebase/firestore';
+
 import '../style/wpisaniePinuStyl.css';
 
 function WpisaniePinu(){
-    const [pin, setPin] = useState('');
+    const navigation = useNavigate();
+    const [pin, setPin] = useState(0);
+    const [komunikat, setKomunikat] = useState('');
+    const [proby, setProby] = useState(0);
+
+    const doStrony = () => {
+        navigation("/4");
+    }
+
+    const tooManyTry = () => {
+        if(proby == 2){
+            setKomunikat("Sola nawet nie probuj🤫");
+        }
+    }
+
+    async function sprawdzaniePinu(){
+        const q = query(collection(db, "hasla"), where("pin", "==", pin));
+        const querySnapshot = await getDocs(q);
+        const tab = [];
+
+        querySnapshot.forEach((doc) => {
+            tab.push(doc.id);
+        });
+
+        if(tab.length == 1){
+            doStrony();
+        }else{
+            setKomunikat("Zle haslo");
+            setProby(proby + 1);
+            tooManyTry();
+        }
+    }
 
     const filtrowanie = (event) => {
         const value = event.target.value;
-        if(!isNaN(value)){
-            setPin(Number(value));
-        }else{
-            console.log("nop");
-            // dopisac komunakt ktory bedzie sie wypisywal po wpisaniu tekstu
+        setKomunikat('');
+        if(value.length == 4){
+            if(!isNaN(value)){
+                setPin(Number(value));
+                sprawdzaniePinu();
+            }else{
+                setKomunikat("wpisz numer");
+            }
         }
-        
     }
 
     return(
         <div className="pin-container">
-            <h1>Wprowadź PIN</h1>
+            <div>
+                <h1>Wprowadź PIN</h1>
+                <input type="text" maxLength="4" onChange={filtrowanie} inputMode="numeric"/>
 
-            <input type="text" maxLength="4"  onChange={filtrowanie} inputMode="numeric"/>
-          
+                {komunikat.length > 1 && <h3>{komunikat}</h3>}
+            </div>
         </div>
     )
 }
--- app/screens/Grafik.jsx
+++ app/screens/Grafik.jsx
@@ -1,12 +1,113 @@
+import React, { useState } from 'react';
+import { format, eachDayOfInterval, startOfMonth, endOfMonth, getDay, getDate } from 'date-fns';
+
 import '../style/grafikStyl.css';
+import { tr } from 'date-fns/locale';
+
+function Kalendarz(){
+    const [startWeek, setStartWeek] = useState(true);
+
+    const obecnaData = new Date();
+    const dniWTygodniu = eachDayOfInterval({
+        start: startOfMonth(obecnaData),
+        end: endOfMonth(obecnaData)
+    });
+
+    const numerDnia = (data) => {
+        return getDay(data);
+    }
+
+    const ukladanie = () => {
+        const dniRows = [];
+        let dniColumns = [];
+        let currentDayIndex = 0;
+
+        dniWTygodniu.forEach((day, index) => {
+            if (index === 0) {
+                for (let i = 0; i < numerDnia(day); i++) {
+                    dniColumns.push(<td key={`empty-${i}`}></td>);
+                    currentDayIndex++;
+                }
+            }
+
+            dniColumns.push(<td key={day}>{getDate(day)}</td>);
+            currentDayIndex++;
+
+            if (currentDayIndex === 7) {
+                dniRows.push(<tr key={`row-${index}`}>{dniColumns}</tr>);
+                dniColumns = [];
+                currentDayIndex = 0;
+            }
+        });
+
+        if (dniColumns.length > 0) {
+            while (dniColumns.length < 7) {
+                dniColumns.push(<td key={`empty-${dniColumns.length}`}></td>);
+            }
+            dniRows.push(<tr key={`row-last`}>{dniColumns}</tr>);
+        }
+
+        return dniRows;
+    }
+
+    return (
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Poniedziałek</th>
+                        <th>Wtorek</th>
+                        <th>Środa</th>
+                        <th>Czwartek</th>
+                        <th>Piątek</th>
+                        <th>Sobota</th>
+                        <th>Niedziela</th>
+                    </tr>
+                    </thead>
+
+                <tbody>
+                    {ukladanie()}
+                </tbody>
+
+            </table>
+        </div>
+    );
+}
+
+//     const now = new Date();
+//   const daysInMonth = eachDayOfInterval({
+//     start: startOfMonth(now),
+//     end: endOfMonth(now)
+//   });
+
+//   const getWeekDayNumber = (date) => {
+//     const day = getDay(date); // 0 (Sunday) - 6 (Saturday)
+//     return day; // Convert 0 (Sunday) to 7
+//   };
+
+//   return (
+//     <div>
+//       <h1>Days of the current month</h1>
+//       <ul>
+//         {daysInMonth.map(day => (
+//           <li key={day}>
+//             {`${getWeekDayNumber(day)}, ${format(day, 'd, M, yyyy')}`}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
 
 function Grafik(){
 
     return(
         <div>
-            <h1>Grafik</h1>
+           <h1>Kalendarz</h1>
+
+            <Kalendarz />
+
         </div>
-    )
+    );
 }
 
 export default Grafik;
